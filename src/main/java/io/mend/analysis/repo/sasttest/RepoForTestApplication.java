05efcab35cc04ce4598cc4fa10b5ae35fd39ebc4
sast-qa <123078419+sast-qa@users.noreply.github.com>
2023-06-02 12:02:29 +0200

    Update RepoForTestApplication.java

--- src/main/java/io/mend/analysis/repo/sasttest/RepoForTestApplication.java
+++ src/main/java/io/mend/analysis/repo/sasttest/RepoForTestApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RepoForTestApplication {
-    
+public class RepoForTestApplication {    
     
   public static void main(String[] args) {
     SpringApplication.run(RepoForTestApplication.class, args);
